0f7102d6491e25fc3058b4b411655c1860434585
绪南 <593855482@qq.com>
2024-09-19 18:36:29 +0800

    Time: 71 ms (61.07%), Space: 57 MB (48.09%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.ts
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.ts
@@ -20,6 +20,12 @@ function connect(root: _Node | null): _Node | null {
     }
 
     // 因为一个节点解决不了问题，所以要用两个节点
+    // 分析：
+    // 1. 根节点要做的事情是连接左右两个字节点，但是发现存在跨父节点，明显一个节点解决不了问题
+    // 2. 所以，那就要用到两个节点来解决问题了，说明需要两个参数，所以另起一个递归函数来处理
+    // 3. 接下来就要考虑函数的功能：把相邻的两个节点连接起来
+    // 4. 这里已经不依托于二叉树的遍历了
+
     next(root.left, root.right);
 
 
@@ -33,7 +39,6 @@ function next(node1: _Node, node2: _Node) {
 
     node1.next = node2;
     next(node1.left, node1.right);
-    next(node2.left, node2.right);
-
     next(node1.right, node2.left);
+    next(node2.left, node2.right);
 }
