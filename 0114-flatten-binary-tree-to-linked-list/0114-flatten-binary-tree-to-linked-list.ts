d006be4fad21b6f07f363f6712cb8dbb84fea862
绪南 <593855482@qq.com>
2024-09-22 22:45:07 +0800

    Time: 73 ms (54.32%), Space: 53.3 MB (78.19%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.ts
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.ts
@@ -15,26 +15,44 @@
 /**
  Do not return anything, modify root in-place instead.
  */
-
-const queue = [];
-
+// 注意不要用全局变量！！执行的时候变量不会重置
 function flatten(root: TreeNode | null): void {
+    // const queue = [];
     // 非递归写法，先前序遍历储存然后重建
-    traverse(root);
-    while(queue.length > 0) {
-        const p = queue.shift();
-        p.left = null;
-        if (queue[0]) {
-            p.right = queue[0];
-        }
-    }
-};
+    // function traverse(root: TreeNode) {
+    //     if (root === null) {
+    //         return;
+    //     }
+    //     queue.push(root);
+    //     traverse(root.left);
+    //     traverse(root.right);
+    // }
+    // traverse(root);
+    // while(queue.length > 0) {
+    //     const p = queue.shift();
+    //     p.left = null;
+    //     if (queue[0]) {
+    //         p.right = queue[0];
+    //     }
+    // }
 
-function traverse(root: TreeNode) {
+    // 递归写法，后序遍历的变种 左右中->右左中
     if (root === null) {
         return;
     }
-    queue.push(root);
-    traverse(root.left);
-    traverse(root.right);
-}
+    let p = null;
+
+    function walk(root) {
+        if (root === null) {
+            return;
+        }
+        walk(root.right);
+        walk(root.left);
+
+        root.right = p;
+        root.left = null;
+        p = root;
+    }
+
+    walk(root);
+};
