04b9eb4918b8eaaeb584faf7a656302ecbcbc10b
绪南 <593855482@qq.com>
2024-09-23 13:22:52 +0800

    Time: 118 ms (59.76%), Space: 125.9 MB (46.48%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -16,10 +16,11 @@ function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
         return null;
     }
 
+    // 先根节点，然后构建左右子树
     const val = preorder[0];
     const root = new TreeNode(val);
     const index = inorder.indexOf(val);
-
+    // 关键在于构建子树的时候每次需要的数组总是不一样的，前提是要能识别到根
     root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
     root.right = buildTree(preorder.slice(index + 1, preorder.length), inorder.slice(index+1));
     return root;
