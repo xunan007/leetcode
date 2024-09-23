0e7647d90be6417bd407aeded706e96ef808336a
绪南 <593855482@qq.com>
2024-09-23 18:22:05 +0800

    Time: 116 ms (30.64%), Space: 62.6 MB (58.72%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.ts
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.ts
@@ -16,20 +16,41 @@
  * Encodes a tree to a single string.
  */
 function serialize(root: TreeNode | null): string {
-    
-    let s: string = '';
-    const fn = (root: TreeNode | null) => {
-        if (root === null) {
-            s += 'null,'
-            return;
-        }
-        // 前序遍历的操作
-        s += `${root.val},`;
-        fn(root.left);
-        fn(root.right);
-    }
+    let s = '';
 
-    fn(root);
+    // 1. 前序遍历的方式
+    // const fn = (root: TreeNode | null) => {
+    //     if (root === null) {
+    //         s += 'null,'
+    //         return;
+    //     }
+    //     // 前序遍历的操作
+    //     s += `${root.val},`;
+    //     fn(root.left);
+    //     fn(root.right);
+    // }
+
+    // fn(root);
+
+    // return s.slice(0, s.length - 1);
+
+    // 2. 层序遍历的方式
+    if (root === null) {
+        return 'null';
+    }
+    
+    let arr = [];
+    arr.push(root);
+    while(arr.length > 0) {
+        const node = arr.shift();
+        if (node === null) {
+            s += 'null,';
+            continue;
+        }
+        arr.push(node.left);
+        arr.push(node.right);
+        s += `${node.val},`;
+    }
 
     return s.slice(0, s.length - 1);
 };
@@ -40,21 +61,48 @@ function serialize(root: TreeNode | null): string {
 function deserialize(data: string): TreeNode | null {
     let list = data.split(',');
 
-    const fn = (list: string[]): TreeNode | null => {
-        const val = list.shift();
+    // 1. 前序遍历的方式
+    // const fn = (list: string[]): TreeNode | null => {
+    //     const val = list.shift();
 
-        if (val === 'null') {
-            return null;
-        }
+    //     if (val === 'null') {
+    //         return null;
+    //     }
 
-        const root = new TreeNode(Number(val));
-        root.left = fn(list);
-        root.right = fn(list);
+    //     const root = new TreeNode(Number(val));
+    //     root.left = fn(list);
+    //     root.right = fn(list);
 
-        return root;
+    //     return root;
+    // }
+
+    // return fn(list);
+
+    // 2. 层序遍历的方式
+    const val = list[0];
+    if (val === 'null') {
+        return null;
     }
-
-    return fn(list);
+    const root = new TreeNode(Number(val));
+    let queue = [root];
+    // 这里的巧妙之处就在于通过 index 偏移去记录叶子节点的起始位置
+    let index = 1;
+    while(queue.length > 0) {
+        const node = queue.shift();
+        if (list[index] !== 'null') {
+            const left = new TreeNode(Number(list[index]));
+            node.left = left;
+            queue.push(left);
+        }
+        index++;
+        if (list[index] !== 'null') {
+            const right = new TreeNode(Number(list[index]));
+            node.right = right;
+            queue.push(right);
+        }
+        index++;
+    }
+    return root;
 };
 
 
