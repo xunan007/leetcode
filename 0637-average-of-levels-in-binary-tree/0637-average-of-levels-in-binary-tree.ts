bf447daa075ecaa3434c9e6ebc88c389df239f5d
绪南 <593855482@qq.com>
2024-09-25 17:54:02 +0800

    Time: 69 ms (71.16%), Space: 55.2 MB (74.29%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function averageOfLevels(root: TreeNode | null): number[] {
+    let result = [];
+    if (root === null) {
+        return result;
+    }
+    const queue = [root];
+    while (queue.length > 0) {
+        const len = queue.length;
+        let sum = 0;
+        for (let i = 0; i < len; i++) {
+            const node = queue.shift();
+            sum += node.val;
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+        result.push(sum / len);
+    }
+    return result;
+};
