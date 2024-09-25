1e58c0cc960bbf3c307f926269d3495bf65630ff
绪南 <593855482@qq.com>
2024-09-25 18:38:01 +0800

    Time: 64 ms (89.13%), Space: 57.5 MB (12.32%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.ts
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.ts
@@ -15,22 +15,47 @@
  */
 
 function connect(root: _Node | null): _Node | null {
+    // 1. 遍历 -> 递归的方式
     // 对根节点的判空
-    if (root == null) {
+    // if (root == null) {
+    //     return null;
+    // }
+
+    // // 这个是递归结束的位置
+    // if (root.left === null) {
+    //     return root;
+    // }
+
+    // //  前序操作，根节点需要做两件事情
+    // root.left.next = root.right;
+    // root.right.next = root.next?.left;
+    // // 还是前序遍历的框架
+    // connect(root.left);
+    // connect(root.right);
+
+    // return root;
+
+    // 2. 遍历 -> 迭代的方式
+    if (root === null) {
         return null;
     }
 
-    // 这个是递归结束的位置
-    if (root.left === null) {
-        return root;
+    let queue = [root];
+    while (queue.length > 0) {
+        const len = queue.length;
+        for (let i = 0; i < len; i++) {
+            const node = queue.shift();
+            if (queue[0] && i !== len - 1) {
+                node.next = queue[0];
+            }
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
     }
-
-    //  前序操作，根节点需要做两件事情
-    root.left.next = root.right;
-    root.right.next = root.next?.left;
-    // 还是前序遍历的框架
-    connect(root.left);
-    connect(root.right);
 
     return root;
 };
