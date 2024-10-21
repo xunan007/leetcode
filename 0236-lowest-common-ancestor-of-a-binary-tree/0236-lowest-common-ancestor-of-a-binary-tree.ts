09169071762339fe98f972d0088fb5dfeb486d5e
绪南 <593855482@qq.com>
2024-10-21 16:46:46 +0800

    Time: 83 ms (21.58%), Space: 59.5 MB (71.88%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
@@ -18,8 +18,9 @@ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: Tree
     }
     // 1. 遍历解决
     const walk = (root: TreeNode | null): TreeNode | null => {
-        if (root === null) {
-            return null;
+        // 找的位置在这里写，是因为如果找到相等，这个节点要么就是父节点，要么就是自身，子树不需要再去处理了，因为这两个节点默认是都在的
+        if (root === null || root === p || root === q) {
+            return root;
         }
         const a = walk(root.left);
         const b = walk(root.right);
@@ -30,9 +31,10 @@ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: Tree
         }
         // 一边有结果，直接向上返回
         if (a || b) {
-            if (root.val === p.val || root.val === q.val) {
-                return root;
-            }
+            // 这段代码不需要了
+            // if (root.val === p.val || root.val === q.val) {
+            //     return root;
+            // }
             if (a) {
                 return a;
             }
@@ -40,14 +42,37 @@ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: Tree
                 return b;
             }
         }
-        // 左右都没记录，那就找
-        if (root.val === p.val || root.val === q.val) {
-            return root;
-        }
+        // 找的逻辑不用在后序这里写
+        // if (root.val === p.val || root.val === q.val) {
+        //     return root;
+        // }
         // 都没有，那就返回 null
         return null;
     }
     return walk(root);
-    // 2. 分解问题
+    // // 2. 分解问题
+    // let l = lowestCommonAncestor(root.left, p, q);
+    // let r = lowestCommonAncestor(root.right, p, q);
 
+    // if (l && r) {
+    //     return root;
+    // }
+
+    // if (l || r) {
+    //     if (root.val === p.val || root.val === q.val) {
+    //         return root;
+    //     }
+    //     if (l) {
+    //         return l;
+    //     }
+    //     if (r) {
+    //         return r;
+    //     }
+    // }
+
+    // if (root.val === p.val || root.val === q.val) {
+    //     return root;
+    // }
+
+    // return null;
 };
