cc3a3652782c3f94d24a7ba978b44d891ae7c4e4
绪南 <593855482@qq.com>
2024-10-22 22:30:51 +0800

    Time: 1 ms (99.45%), Space: 53.5 MB (56.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.ts
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -1,20 +1,21 @@
 function mySqrt(x: number): number {
     let l = 0;
     let r = x;
-    let ans = -1;
     while (l <= r) {
         let mid = Math.floor((l + r) / 2);
         if (mid * mid === x) {
-            ans = mid;
-            return ans;
+            return mid;
         }
         if (mid * mid < x) {
-            ans = mid;
             l = mid + 1;
         }
         if (mid * mid > x) {
             r = mid - 1;
         }
     }
-    return ans;
+    if (r <= x) {
+        return r;
+    } else {
+        return -1;
+    }
 };
