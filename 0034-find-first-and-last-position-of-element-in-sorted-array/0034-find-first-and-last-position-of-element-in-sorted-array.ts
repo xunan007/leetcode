bd70fc176220de678887a829bd1bfbf14732b994
绪南 <593855482@qq.com>
2024-10-22 17:10:54 +0800

    Time: 0 ms (100%), Space: 51.7 MB (81.72%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
@@ -0,0 +1,60 @@
+function searchRange(nums: number[], target: number): number[] {
+    // 找右边第一个比它大的，找左边第一个比它小的；当然如果这都找不到，这也是空
+    // 然后两者的区间如果还有值，那铁定就是对的，否则就是空的
+    // 两次二分查找复杂度也是logN
+    const lb = getLeftBorder(nums, target);
+    if (lb === -2) return [-1, -1];
+    const rb = getRightBorder(nums, target);
+    if (rb === -2) return [-1, -1];
+    if (rb - lb > 1) {
+        return [lb + 1, rb - 1];
+    }
+    return [-1, -1];
+
+};
+
+// 找第一个比 target 小的
+function getLeftBorder(nums, target): number {
+    let left = 0;
+    let right = nums.length - 1;
+    if (nums[left] > target) {
+        return -2;
+    }
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        // 这里的比较逻辑有难度
+        if (target === nums[mid]) {
+            right = mid - 1;
+        }
+        if (target < nums[mid]) {
+            right = mid - 1;
+        }
+        if (target > nums[mid]) {
+            left = mid + 1;
+        }
+    }
+    return right;
+}
+
+// 找第一个比target 大的
+function getRightBorder(nums, target): number {
+    let left = 0;
+    let right = nums.length - 1;
+    if (nums[right] < target) {
+        return -2;
+    }
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        // 这里的比较逻辑有难度
+        if (target === nums[mid]) {
+            left = mid + 1;
+        }
+        if (target < nums[mid]) {
+            right = mid - 1;
+        }
+        if (target > nums[mid]) {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
