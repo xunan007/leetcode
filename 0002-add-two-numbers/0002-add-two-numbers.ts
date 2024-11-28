6117f3a03409de8df5654996f8f6313a06967a48
绪南 <593855482@qq.com>
2024-11-28 16:59:30 +0800

    Time: 7 ms (30.94%), Space: 57.4 MB (75.14%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let dummy = new ListNode(0);
+    let p = dummy;
+    let left = 0;
+    while (l1 || l2) {
+        let sum = left;
+        if (l1) {
+            sum += l1.val;
+        }
+        if (l2) {
+            sum += l2.val;
+        }
+        // 判断是否需要进位
+        if (sum >= 10) {
+            left = 1;
+        } else {
+            left = 0;
+        }
+
+        p.next = new ListNode(sum % 10);
+        p = p.next;
+
+        if (l1) {
+            l1 = l1.next;
+        }
+        if (l2) {
+            l2 = l2.next;
+        }
+    }
+
+    if (left === 1) {
+        p.next = new ListNode(1);
+    }
+
+    return dummy.next;
+};
