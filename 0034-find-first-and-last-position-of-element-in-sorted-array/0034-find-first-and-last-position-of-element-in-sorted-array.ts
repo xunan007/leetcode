8dcfbf134f5d04d2303d6d6124e28620069d0504
绪南 <593855482@qq.com>
2024-12-11 21:32:45 +0800

    Time: 0 ms (100%), Space: 52.2 MB (45.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,64 +1,28 @@
 function searchRange(nums: number[], target: number): number[] {
-    // 找右边第一个比它大的，找左边第一个比它小的；当然如果这都找不到，这也是空
-    // 然后两者的区间如果还有值，那铁定就是对的，否则就是空的
-    // 两次二分查找复杂度也是logN
-    const lb = getLeftBorder(nums, target);
-    if (lb === -2) return [-1, -1];
-    const rb = getRightBorder(nums, target);
-    if (rb === -2) return [-1, -1];
-    if (rb - lb > 1) {
-        return [lb + 1, rb - 1];
+    // 找到第一个>=X的数
+    const search = (nums: number[], target: number): number => {
+        let left = 0;
+        let right = nums.length - 1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
-    return [-1, -1];
-
+    const left = search(nums, target);
+    // 存在两种情况，一是所有的数都比target小，那么left溢出
+    // 要么所有的数比left大或者left压根就不存在
+    if (left === nums.length || nums[left] !== target) {
+        return [-1, -1];
+    }
+    // 找到最后一个<=X等价于找到第一个>=X+1的数的左边
+    // 注意有left一定就有right
+    const right = search(nums, target + 1) - 1;
+    return [left, right];
 };
 
-// 下面那些 if else 到底取左还是取右，列个简单的例子辅助解决就可以了
 
-// 找第一个比 target 小的
-// target 左边的最大，right要远离这个target
-function getLeftBorder(nums, target): number {
-    let left = 0;
-    let right = nums.length - 1;
-    if (nums[left] > target) {
-        return -2;
-    }
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        // 这里的比较逻辑有难度
-        if (target === nums[mid]) {
-            right = mid - 1; // 关键代码
-        }
-        if (target < nums[mid]) {
-            right = mid - 1;
-        }
-        if (target > nums[mid]) {
-            left = mid + 1;
-        }
-    }
-    return right;
-}
-
-// 找第一个比target 大的
-// target 右边的最小，left 要远离这个target
-function getRightBorder(nums, target): number {
-    let left = 0;
-    let right = nums.length - 1;
-    if (nums[right] < target) {
-        return -2;
-    }
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        // 这里的比较逻辑有难度
-        if (target === nums[mid]) {
-            left = mid + 1; // 关键代码
-        }
-        if (target < nums[mid]) {
-            right = mid - 1;
-        }
-        if (target > nums[mid]) {
-            left = mid + 1;
-        }
-    }
-    return left;
-}
