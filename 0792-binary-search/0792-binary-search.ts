39f5fd4d1766668dbfb77d187935e7db8becf788
绪南 <593855482@qq.com>
2024-12-16 15:54:26 +0800

    Time: 0 ms (100%), Space: 53.5 MB (69.74%) - LeetHub

--- 0792-binary-search/0792-binary-search.ts
+++ 0792-binary-search/0792-binary-search.ts
@@ -1,18 +1,35 @@
 function search(nums: number[], target: number): number {
-    let left = 0;
-    let right = nums.length - 1;
-    while(left <= right) {
-        let middle = Math.floor((left + right) / 2);
+    // 闭区间
+    // let left = 0;
+    // let right = nums.length - 1;
+    // while(left <= right) {
+    //     let middle = Math.floor((left + right) / 2);
+    //     if (nums[middle] === target) {
+    //         return middle;
+    //     }
+    //     if (nums[middle] < target) {
+    //         left = middle + 1;
+    //     }
+    //     if (nums[middle] > target) {
+    //         right = middle - 1;
+    //     }
+    // }
+    // return -1;
+
+    // 开区间
+    let left = -1;
+    let right = nums.length;
+    while (left + 1 < right) {
+        let middle = Math.floor((left+right)/2);
         if (nums[middle] === target) {
             return middle;
         }
         if (nums[middle] < target) {
-            left = middle + 1;
+            left = middle;
         }
         if (nums[middle] > target) {
-            right = middle - 1;
+            right = middle;
         }
     }
     return -1;
 };
-
