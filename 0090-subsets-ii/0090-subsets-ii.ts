0bd3f7298affb6f968f28be65b18b41d2d936830
绪南 <593855482@qq.com>
2024-12-27 17:41:16 +0800

    Time: 2 ms (47.64%), Space: 54.6 MB (72.77%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -0,0 +1,18 @@
+function subsetsWithDup(nums: number[]): number[][] {
+    let result: number[][] = [];
+    let path: number[] = [];
+    nums.sort((a, b) => a - b);
+    const backtracking = (startIdx: number) => {
+        result.push(path.slice());
+        for (let i = startIdx; i < nums.length; i++) {
+            if (i !== startIdx && nums[i] === nums[i - 1]) {
+                continue;
+            }
+            path.push(nums[i]);
+            backtracking(i + 1);
+            path.pop();
+        }
+    }
+    backtracking(0);
+    return result;
+};
