9fc23cd22d9e501bd6dd814084839d82f27ac0b4
绪南 <593855482@qq.com>
2024-12-30 16:04:07 +0800

    Time: 548 ms (42.71%), Space: 57.3 MB (14.07%) - LeetHub

---
+++ 0079-word-search/0079-word-search.ts
@@ -0,0 +1,50 @@
+function exist(board: string[][], word: string): boolean {
+    let maxY = board.length;
+    let maxX = board[0].length;
+    let used: number[][] = [];
+    for (let i = 0; i < maxY; i++) {
+        used.push(new Array(maxX).fill(0));
+    }
+    const canFind = (startX: number, startY: number, word: string): boolean => {
+        used[startY][startX] = 1;
+        // 没有需要找的字符了，说明结束了
+        if (word.length === 0) {
+            return true;
+        }
+        const find: number[][] = [
+            [startX, startY - 1],
+            [startX, startY + 1],
+            [startX - 1, startY],
+            [startX + 1, startY],
+        ].filter((item) => {
+            // 去掉不合法的，要符合没有被用过且不要溢出
+            const x = item[0];
+            const y = item[1];
+            return x >= 0 && x < maxX && y >= 0 && y < maxY && used[y][x] === 0;
+        });
+        // 没有可用的位置了，找失败了
+        if (find.length === 0) {
+            used[startY][startX] = 0;
+            return false;
+        }
+        for (let i = 0; i < find.length; i++) {
+            const [x, y] = find[i];
+            if (board[y][x] === word[0] && canFind(x, y, word.slice(1))) {
+                return true;
+            }
+        }
+        used[startY][startX] = 0;
+        return false;
+
+    };
+
+    for (let i = 0; i < maxX; i++) {
+        for (let j = 0; j < maxY; j++) {
+            if (board[j][i] === word[0] && canFind(i, j, word.slice(1))) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
