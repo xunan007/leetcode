cc311c8faeda90e34df401a21db5ce82f558fcac
绪南 <593855482@qq.com>
2024-12-31 10:51:24 +0800

    Time: 4 ms (34.52%), Space: 52.4 MB (44.96%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -0,0 +1,29 @@
+function jump(nums: number[]): number {
+    if (nums.length === 1) {
+        return 0;
+    }
+    let cover = nums[0] + 1;
+    let maxCover = nums.length;
+    let path: number[] = [cover];
+    if (cover >= maxCover) {
+        return path.length;
+    }
+    for (let i = 0; i < maxCover && i < path[path.length - 1]; i++) {
+        cover = Math.max(i + 1 + nums[i], cover);
+        if (cover >= maxCover) {
+            path.push(cover);
+            break;
+        }
+        // 下一步能走到的最大值已经定下来了，那么第二步就可以先跳过去了
+        if (i === path[path.length - 1] - 1) {
+            path.push(cover);
+        }
+    }
+    return path.length;
+};
+
+// [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
+//  1             .
+//      1                 .
+//          1                 .
+//                1.            .
