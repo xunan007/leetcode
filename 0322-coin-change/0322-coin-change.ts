ddf6643a43693b112436b4235ddcff8b0064616e
绪南 <593855482@qq.com>
2025-01-01 16:32:30 +0800

    Time: 26 ms (93.38%), Space: 56 MB (75.98%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.ts
@@ -0,0 +1,15 @@
+function coinChange(coins: number[], amount: number): number {
+    let dp: number[] = [];
+    dp[0] = 0;
+    for (let i = 1; i <= amount; i++) {
+        let min = Infinity;
+        for (let j = 0; j < coins.length; j++) {
+            let coin = coins[j];
+            if (i - coin >= 0 && dp[i - coin] !== -1) {
+                min = Math.min(min, dp[i - coin] + 1);
+            }
+        }
+        dp[i] = min === Infinity ? -1 : min;
+    }
+    return dp[amount];
+};
