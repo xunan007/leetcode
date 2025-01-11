b7ff292b1000a68ddc996b4d5cc0ff0cc050f783
绪南 <593855482@qq.com>
2025-01-11 17:41:04 +0800

    Time: 50 ms (95.24%), Space: 51.9 MB (100%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.ts
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.ts
@@ -1,20 +1,39 @@
 function findLength(nums1: number[], nums2: number[]): number {
-    let dp: number[][] = [];
+    // let dp: number[][] = [];
+    // let result: number = 0;
+    // for (let i = 0; i < nums1.length; i++) {
+    //     let arr = new Array(nums2.length).fill(0);
+    //     dp.push(arr);
+    // }
+    // for (let i = 0; i < nums1.length; i++) {
+    //     for (let j = 0; j < nums2.length; j++) {
+    //         if (nums1[i] === nums2[j]) {
+    //             // i-1和j-1有可能是负的，这种情况直接当作0来处理就行
+    //             if (i === 0 || j === 0) {
+    //                 dp[i][j] = 1;
+    //             } else {
+    //                 dp[i][j] = dp[i - 1][j - 1] + 1;
+    //             }
+    //             result = Math.max(result, dp[i][j]);
+    //         }
+    //     }
+    // }
+    // return result;
+    // 二维降一维
     let result: number = 0;
+    let dp: number[] = new Array(nums2.length).fill(0);
     for (let i = 0; i < nums1.length; i++) {
-        let arr = new Array(nums2.length).fill(0);
-        dp.push(arr);
-    }
-    for (let i = 0; i < nums1.length; i++) {
-        for (let j = 0; j < nums2.length; j++) {
+        for (let j = nums2.length - 1; j >= 0; j--) {
             if (nums1[i] === nums2[j]) {
-                // i-1和j-1有可能是负的，这种情况直接当作0来处理就行
                 if (i === 0 || j === 0) {
-                    dp[i][j] = 1;
+                    dp[j] = 1;
                 } else {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[j] = dp[j - 1] + 1;
                 }
-                result = Math.max(result, dp[i][j]);
+                result = Math.max(result, dp[j]);
+            } else {
+                // 这里很重要，必须还原初始值
+                dp[j] = 0;
             }
         }
     }
