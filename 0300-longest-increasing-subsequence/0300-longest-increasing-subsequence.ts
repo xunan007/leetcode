37473d36da0cbf738d9eab947d785db99561c921
绪南 <593855482@qq.com>
2025-01-11 11:05:55 +0800

    Time: 92 ms (25.62%), Space: 51.9 MB (87.87%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
@@ -0,0 +1,11 @@
+function lengthOfLIS(nums: number[]): number {
+    let dp: number[] = new Array(nums.length).fill(1);
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    return Math.max(...dp);
+};
