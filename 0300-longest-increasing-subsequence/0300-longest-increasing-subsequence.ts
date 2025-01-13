0dac0a2f655da673bc6c2dc3268f879699faa2e5
绪南 <593855482@qq.com>
2025-01-13 18:45:40 +0800

    Time: 90 ms (32.53%), Space: 52.3 MB (54.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
@@ -1,11 +1,33 @@
 function lengthOfLIS(nums: number[]): number {
-    let dp: number[] = new Array(nums.length).fill(1);
-    for (let i = 1; i < nums.length; i++) {
+    // 1. 递归+记忆化
+    // let cache: number[] = [];
+    // const dfs = (i: number): number => {
+    //     if (cache[i] !== undefined) {
+    //         return cache[i];
+    //     }
+    //     let result: number = 0;
+    //     for (let j = 0; j < i; j++) {
+    //         if (nums[j] < nums[i]) {
+    //             result = Math.max(dfs(j), result);
+    //         }
+    //     }
+    //     cache[i] = result + 1;
+    //     return result + 1;
+    // }
+    // let result = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     result = Math.max(dfs(i), result);
+    // }
+    // return result;
+    // 2. 递推
+    let dp: number[] = new Array(nums.length).fill(0);
+    for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
-                dp[i] = Math.max(dp[i], dp[j] + 1);
+                dp[i] = Math.max(dp[j], dp[i])
             }
         }
+        dp[i] = dp[i] + 1;
     }
     return Math.max(...dp);
 };
