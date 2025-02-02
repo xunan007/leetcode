7cec55fe27d409ce6d7ea76557e0a8f882ad04d0
绪南 <593855482@qq.com>
2025-02-02 18:06:52 +0800

    Time: 0 ms (100%), Space: 55.1 MB (38.86%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.ts
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -4,25 +4,36 @@ function subsetsWithDup(nums: number[]): number[][] {
     // 先排序，方便去重
     nums.sort((a, b) => a - b);
     // INPUT角度
-    const dfs = (i: number) => {
-        if (i === nums.length) {
-            result.push(path.slice());
-            return;
-        }
-        // 不选
-        let j = i+1;
-        // 去重逻辑
-        while(nums[j] === nums[i]) {
-            j++;
-        }
-        dfs(j);
-        // 选
-        path.push(nums[i]);
-        dfs(i+1);
-        path.pop();
-    }
+    // const dfs = (i: number) => {
+    //     if (i === nums.length) {
+    //         result.push(path.slice());
+    //         return;
+    //     }
+    //     // 不选
+    //     let j = i+1;
+    //     // 去重逻辑
+    //     while(nums[j] === nums[i]) {
+    //         j++;
+    //     }
+    //     dfs(j);
+    //     // 选
+    //     path.push(nums[i]);
+    //     dfs(i+1);
+    //     path.pop();
+    // }
     // ANSWER角度
-    
+    const dfs = (i: number) => {
+        result.push(path.slice());
+        for (let j = i; j < nums.length; j++) {
+            if (j !== i && nums[j] === nums[j - 1]) {
+                continue;
+            }
+            path.push(nums[j]);
+            dfs(j + 1);
+            path.pop();
+        }
+    }
+
     dfs(0);
     return result;
 };
