fcc2640069bcbbbf439932d07fe4912952f184d2
绪南 <593855482@qq.com>
2025-02-04 11:44:10 +0800

    Time: 5 ms (19.58%), Space: 51.8 MB (53.06%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.ts
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -1,30 +1,37 @@
 function generateParenthesis(n: number): string[] {
     let result: string[] = [];
     let path: string[] = [];
-    let l = n;
-    let r = n;
-    const backtracking = () => {
-        if (l === 0 && r === 0) {
-            result.push(path.join(''));
+
+    // 需要的括号数
+    let nl = n;
+    let nr = 0;
+
+    const dfs = (): void => {
+        if (path.length === n * 2) {
+            result.push(path.slice().join(''));
             return;
         }
-        for (let i = 0; i < 2; i++) {
-            if (i === 0 && l === 0) {
-                continue;
-            }
-            if (i === 1 && r === 0) {
-                continue;
-            }
-            if (i === 1 && r - 1 < l) {
-                continue;
-            }
-            path.push(i === 0 ? '(' : ')');
-            i === 0 ? l-- : r--;
-            backtracking();
+
+        if (nl > 0) {
+            path.push('(');
+            nl--;
+            nr++;
+            dfs();
             path.pop();
-            i === 0 ? l++ : r++;
+            nl++;
+            nr--;
         }
-    };
-    backtracking();
+
+        if (nr > 0) {
+            path.push(')');
+            nr--;
+            dfs();
+            path.pop();
+            nr++;
+        }
+
+    }
+
+    dfs();
     return result;
 }
