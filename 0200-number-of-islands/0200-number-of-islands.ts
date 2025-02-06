2f8c6a5e5800717b0b2cbe461e768d754f2a4012
绪南 <593855482@qq.com>
2025-02-06 18:27:46 +0800

    Time: 81 ms (59.79%), Space: 53.7 MB (67.88%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -0,0 +1,35 @@
+function numIslands(grid: string[][]): number {
+    let m = grid.length;
+    let n = grid[0].length;
+    let isVisited: boolean[][] = [];
+    for (let i = 0; i < m; i++) {
+        let arr: boolean[] = new Array(n).fill(false);
+        isVisited.push(arr);
+    }
+    const dfs = (y: number, x: number): void => {
+        if (!(y >= 0 && y < m && x >= 0 && x < n)) {
+            return;
+        }
+        if (isVisited[y][x]) {
+            return;
+        }
+        isVisited[y][x] = true;
+        if (grid[y][x] === '0') {
+            return;
+        }
+        dfs(y - 1, x);
+        dfs(y + 1, x);
+        dfs(y, x - 1);
+        dfs(y, x + 1);
+    };
+    let result = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1' && !isVisited[i][j]) {
+                result++;
+                dfs(i, j);
+            }
+        }
+    }
+    return result;
+};
