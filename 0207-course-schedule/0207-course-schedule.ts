42aa2d0d254ff909e4389a03c73c3788e37619c2
绪南 <593855482@qq.com>
2025-02-08 16:48:53 +0800

    Time: 5 ms (99.07%), Space: 55.7 MB (90.56%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.ts
@@ -0,0 +1,35 @@
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    // 参考：https://leetcode.cn/problems/course-schedule/solutions/250377/bao-mu-shi-ti-jie-shou-ba-shou-da-tong-tuo-bu-pai-/?envType=study-plan-v2&envId=top-100-liked
+    // 建临接表和入度数组
+    let g: number[][] = Array.from({ length: numCourses }, () => [])
+    let inDegree: number[] = new Array(numCourses).fill(0);
+    for (let i = 0; i < prerequisites.length; i++) {
+        let [to, from] = prerequisites[i];
+        inDegree[to]++;
+        g[from].push(to);
+    }
+    let queue: number[] = [];
+    let count: number = 0;
+    for (let i = 0; i < inDegree.length; i++) {
+        if (inDegree[i] === 0) {
+            queue.push(i);
+        }
+    }
+    // BFS
+    while (queue.length > 0) {
+        let len = queue.length;
+        for (let i = 0; i < len; i++) {
+            let from = queue.shift();
+            count++;
+            let tos = g[from];
+            for (let j = 0; j < tos.length; j++) {
+                let from = tos[j];
+                inDegree[from]--;
+                if (inDegree[from] === 0) {
+                    queue.push(from);
+                }
+            }
+        }
+    }
+    return count === numCourses;
+};
