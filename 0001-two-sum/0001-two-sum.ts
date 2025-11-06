a4f13fd4846a85dd87ceaf9e500d2675e4df4263
绪南 <593855482@qq.com>
2025-11-06 17:00:13 +0800

    chore: 添加注释
    Add comment to explain handling of duplicate keys in map

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,5 +1,6 @@
 function twoSum(nums: number[], target: number): number[] {
     const mp: Map<number, number> = new Map();
+    // 注意当出现两个相同时，前一个会去覆盖后一个，就是要这么处理
     for (let i = 0; i < nums.length; i++) {
         mp.set(nums[i], i);
     }
