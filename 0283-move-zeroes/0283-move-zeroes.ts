5494f3d46f73868ac7da471fe699edb42e82100b
绪南 <593855482@qq.com>
2025-11-10 17:15:08 +0800

    chore: 添加注释

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -2,6 +2,8 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
+    // 核心就是快慢指针
+    // 难点在于要统一写的操作
     let lowIndex = 0;
     let fastIndex = 0;
     for (; fastIndex < nums.length; fastIndex++) {
