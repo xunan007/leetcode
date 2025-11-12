48bf748e2ed78ba8f400c0a2c559453b07f3eab2
绪南 <593855482@qq.com>
2025-11-12 18:21:15 +0800

    chore: 添加注释

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
@@ -1,15 +1,24 @@
 function subarraySum(nums: number[], k: number): number {
-    let mp: Map<number, number> = new Map();
+    // 讲解：https://leetcode.cn/problems/subarray-sum-equals-k/solutions/247991/dai-ni-da-tong-qian-zhui-he-cong-zui-ben-fang-fa-y
+    // 求连续非空序列，其实就是求区间和
+    // 于是可以转化成前缀和：prefixSum[j]-prefixSum[i-1]=k的情况有多少个
+    // 但这里求 prefixSum 需要一轮循环，然后找个数还需要两层循环
+    // 有没有办法一层循环能够解决问题，如果 prefixSum[i-1] 提前知道了呢
+    // 实际上算 prefixSum[j] 的时候，prefixSum[i-1] 已经算过了，把它存到 map 里面就可以了
     let sum = 0;
     let count = 0;
+    const mp: Map<number, number> = new Map();
     for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
+        sudm += nums[i];
+        // 第一种情况
         if (sum === k) {
             count++;
         }
+        // 第二种情况
         if (mp.has(sum - k)) {
             count += mp.get(sum - k);
         }
+        // 写值
         if (mp.has(sum)) {
             mp.set(sum, mp.get(sum) + 1);
         } else {
