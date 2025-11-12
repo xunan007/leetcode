ace9817b6edd261efc9cf3e9e749accf901a5b28
绪南 <593855482@qq.com>
2025-11-12 21:03:50 +0800

    chore: 添加注释，变更版本

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.ts
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.ts
@@ -1,31 +1,55 @@
 function maxSlidingWindow(nums: number[], k: number): number[] {
-    let queue = new Array();
+    // 难点在于滑动的时候最大值会丢失，这是一个动态的过程
+    // 方法：单调队列
+    // 注意：如果出现滑动窗口满的情况下，下一个要出队的那个刚好就是对头那最大的那个，比如[5,4,3,2]
+    // 题解：https://leetcode.cn/problems/sliding-window-maximum/solutions/667836/dong-hua-yan-shi-dan-diao-dui-lie-239hua-hc5u
     let left = 0;
     let right = 0;
-    let result = [];
-    const push = (item) => {
-        if (queue.length === 0) {
-            queue.push(item);
-        } else {
-            // 注意这里的比较不能用 <=，不然就丢数据了
-            while (queue.length > 0 && queue[queue.length - 1] < item) {
-                queue.pop();
+    let queue: number[] = [];
+    const result: number[] = [];
+    for (let right = 0; right < nums.length; right++) {
+        if (right < k) {
+            let i = queue.length - 1;
+            if (i === -1) {
+                queue.push(nums[right]);
             }
-            queue.push(item);
+            while (i >= 0) {
+                if (queue[i] >= nums[right]) { // 注意这里，两个都相同的情况，也要放进去，不然会丢
+                    queue.push(nums[right]);
+                    break;
+                } else {
+                    queue.pop();
+                    if (queue.length === 0) {
+                        queue.push(nums[right]);
+                    }
+                    i--;
+                }
+            }
+            continue;
         }
-    };
-    const pop = (item) => {
-        if (item === queue[0]) {
+        result.push(queue[0]);
+        if (queue.length === k || queue[0] === nums[left]) { // 注意这里的条件，left超出了但是还是在要去掉
             queue.shift();
         }
-    };
-    for (; right < nums.length; right++) {
-        push(nums[right]);
-        if (right - left + 1 === k) {
-            result.push(queue[0]);
-            pop(nums[left]);
-            left++;
+        left++;
+        let i = queue.length - 1;
+        if (i === -1) {
+            queue.push(nums[right]);
+        }
+        while (i >= 0) {
+            if (queue[i] >= nums[right]) { // 同上
+                queue.push(nums[right]);
+                break;
+            } else {
+                queue.pop();
+                if (queue.length === 0) {
+                    queue.push(nums[right]);
+                }
+                i--;
+            }
         }
     }
+    // 最后还有一个，补上
+    result.push(queue[0]);
     return result;
 };
