4abc3bc8332f66af9c62300ff2b314667cf04c59
绪南 <593855482@qq.com>
2025-11-14 16:57:48 +0800

    chore: 变更版本

--- 0053-maximum-subarray/0053-maximum-subarray.ts
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -15,4 +15,21 @@ function maxSubArray(nums: number[]): number {
         result = Math.max(result, prev);
     }
     return result;
+    // 2025-11-14 前缀和的做法
+    // if (nums.length === 1) {
+    //     return nums[0];
+    // }
+    // // 用前缀和找最大和找最小的
+    // const prefixSum: number[] = new Array(nums.length);
+    // let sum = 0;
+    // let min = 0;
+    // let result = -Infinity;
+    // for (let i = 0; i < nums.length; i++) {
+    //     sum += nums[i];
+    //     prefixSum[i] = sum;
+    //     // 当前值减去前面的最小值
+    //     result = Math.max(sum - min, result);
+    //     min = Math.min(min, sum);
+    // }
+    // return result;
 };
