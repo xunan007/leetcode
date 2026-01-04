f0e6857fb17d3d628643e8b9821f03ca3bb1a053
绪南 <593855482@qq.com>
2026-01-04 16:17:48 +0800

    chore: 添加注释，变更版本

--- 0022-generate-parentheses/0022-generate-parentheses.ts
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -1,37 +1,25 @@
 function generateParenthesis(n: number): string[] {
-    let result: string[] = [];
-    let path: string[] = [];
-
-    // 需要的括号数
-    let nl = n;
-    let nr = 0;
-
-    const dfs = (): void => {
-        if (path.length === n * 2) {
-            result.push(path.slice().join(''));
+    // 站在答案的角度 => 枚举下一个左括号的位置 => 左括号的位置受右括号的影响
+    const result = [];
+    const path = [];
+    // balance=left-right
+    const dfs = (i: number, balance: number) => {
+        if (path.length === n) {
+            const s = new Array(n * 2).fill(')');
+            for (let i = 0; i < path.length; i++) {
+                s[path[i]] = '(';
+            }
+            result.push(s.join(''));
             return;
         }
-
-        if (nl > 0) {
-            path.push('(');
-            nl--;
-            nr++;
-            dfs();
+        for (let right = 0; right <= balance; right++) {
+            // 在当前位置开始补多少个右括号以后可以加左括号
+            path.push(i + right);
+            dfs(i + right + 1, balance - right + 1);
             path.pop();
-            nl++;
-            nr--;
         }
-
-        if (nr > 0) {
-            path.push(')');
-            nr--;
-            dfs();
-            path.pop();
-            nr++;
-        }
-
     }
 
-    dfs();
+    dfs(0, 0);
     return result;
-}
+};
