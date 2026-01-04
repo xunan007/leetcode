af7dcff1dbb8965c2fcd0714d63399909a1db45b
绪南 <593855482@qq.com>
2026-01-04 22:41:43 +0800

    chore: 添加注释，变更版本

--- 0037-sudoku-solver/0037-sudoku-solver.ts
+++ 0037-sudoku-solver/0037-sudoku-solver.ts
@@ -2,53 +2,61 @@
  Do not return anything, modify board in-place instead.
  */
 function solveSudoku(board: string[][]): void {
-    const len = board.length;
-    const isValid = (i: number, j: number, k: string): boolean => {
-        // 遍历这一行/列有没有重复的
-        for (let m = 0; m < len; m++) {
-            if (board[i][m] === k) {
-                return false;
-            }
-            if (board[m][j] === k) {
-                return false;
-            }
-        }
-        // 遍历这个小的3x3有没有重复的
-        let x = 3 * Math.floor(i / 3);
-        let y = 3 * Math.floor(j / 3);
-        for (let m = 0; m < 3; m++) {
-            for (let n = 0; n < 3; n++) {
-                if (board[x + n][y + m] === k) {
-                    return false;
-                }
+    // 用 Set 来去重
+    const options = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const row: Set<string>[] = Array.from({ length: 9 }, () => new Set(options)); // 行
+    const col: Set<string>[] = Array.from({ length: 9 }, () => new Set(options)); // 列
+    const block: Set<string>[] = Array.from({ length: 9 }, () => new Set(options)); // 块
+
+    // 预处理
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const item = board[i][j];
+            const blockIdx = Math.floor(j / 3) + Math.floor(i / 3) * 3;
+            if (item !== '.') {
+                row[i].delete(item);
+                col[j].delete(item);
+                block[blockIdx].delete(item);
             }
         }
-        return true;
     }
-    const backtracking = (): boolean => {
-        // 每一个格都需要判断、填充
-        for (let i = 0; i < len; i++) {
-            for (let j = 0; j < len; j++) {
-                if (board[i][j] !== '.') {
-                    continue;
-                }
-                // 找到需要填充的格，从1到9开始试
-                for (let k = 1; k <= 9; k++) {
-                    if (isValid(i, j, String(k))) {
-                        board[i][j] = String(k);
-                        // 如果有效，进行下一个格子填充
-                        // 理想的情况下可以填充到最后一个格子返回 true，然后上层的所有函数就全部从这里开始 return 了
-                        if (backtracking()) {
-                            return true;
-                        }
-                        board[i][j] = '.';
+
+    const dfs = (i: number, j: number): boolean => {
+        if (i === 9) {
+            return true;
+        }
+
+        // 不为空格，要记录一下 Set
+        const item = board[i][j];
+        const blockIdx = Math.floor(j / 3) + Math.floor(i / 3) * 3;
+        if (item !== '.') {
+            return dfs(j + 1 === 9 ? i + 1 : i, (j + 1) % 9);
+        } else {
+            // 为空格的时候就要开始填了
+            for (let k = 1; k <= 9; k++) {
+                let s = String(k);
+                if (
+                    row[i].has(s)
+                    && col[j].has(s)
+                    && block[blockIdx].has(s)
+                ) {
+                    row[i].delete(s);
+                    col[j].delete(s);
+                    block[blockIdx].delete(s);
+                    board[i][j] = s;
+                    // 注意这里有一个中断
+                    if (dfs(j + 1 === 9 ? i + 1 : i, (j + 1) % 9)) {
+                        return true;
                     }
+                    row[i].add(s);
+                    col[j].add(s);
+                    block[blockIdx].add(s);
+                    board[i][j] = '.';
                 }
-                // 填充到一个位置的时候发现不符合就要返回 false，那么从这一层开始这一条链路就全部回溯失败了
-                return false;
             }
         }
-        return true;
     };
-    backtracking();
+
+    dfs(0, 0);
+    return;
 };
