1a6cec51b924ab72f9a4e03bbfd6866604a8bd6d
绪南 <593855482@qq.com>
2026-01-10 20:32:18 +0800

    chore: 添加注释，变更版本

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -1,47 +1,48 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     // hot100 里面最难的一道题
     // 参考：https://leetcode.cn/problems/median-of-two-sorted-arrays/solutions/2950686/tu-jie-xun-xu-jian-jin-cong-shuang-zhi-z-p2gd
-    // 枚举分组法
-    // 这里调整数组顺序、加入正负无穷的做法非常巧妙
-    // 1. 保证前面数组大小要小于后面
-    if (nums1.length > nums2.length) {
-        [nums1, nums2] = [nums2, nums1];
-    }
+    // 1. 先利用均匀分组，核心“公理”：第一组的最大值小于等于第二组的最小值，只有这种情况是唯一值
+    // max(ai, bj) <= min(ai+1, bj+1) => ai<=ai+1 && bj <= bj+1 => ai<=bj+1 && bj <= ai+1
+    // 2. （记住结论）这个唯一值也暗含着，满足ai<=bj+1&&bj<=ai+1刚好就是满足ai<=bj+1的最大值，所以二分找最大的i就可以了
+    // i的定义是a里面有多少个在第一组，j的定义是b里面有多少个在第一组
     const m = nums1.length;
     const n = nums2.length;
-    // 2. 数组插入负无穷和正无穷，让i和j真正表示他们的含义
+    // 规定个数：第一组>=第二组，所以一定是nums2个数要更多
+    if (m > n) {
+        [nums1, nums2] = [nums2, nums1];
+    }
+    // 这里补充的两个占位很重要，因为i和j的含义不是index，不加那初始就没办法开始了
     nums1 = [-Infinity, ...nums1, Infinity];
     nums2 = [-Infinity, ...nums2, Infinity];
-    // // 3. 初始化i和j，注意这里是拿原始的长度去计算j
-    // let i = 0; // i表示a有多少个数在第一组
-    // let j = Math.floor((m + n + 1) / 2); // j表示b有多少个数在第一组，这么算的含义就是极端情况下第一组的个数总是会比第二组多1个
-    // // 开始比较
+    let i = 0; // a有i个数在第一组
+    let j = Math.floor((m + n + 1) / 2); // b有i个数在第一组，规定第一组比第二组要多1
+
+    // 非二分的做法
     // while (true) {
-    //     if (nums1[i] <= nums2[j + 1] && nums1[i + 1] >= nums2[j]) {
-    //         if ((nums1.length + nums2.length) % 2 === 0) {
-    //             return (Math.max(nums1[i], nums2[j]) + Math.min(nums1[i + 1], nums2[j + 1])) / 2;
-    //         }
-    //         return Math.max(nums1[i], nums2[j]);
+    //     // 核心比较公式
+    //     if (nums1[i] <= nums2[j + 1] && nums2[j] <= nums1[i + 1]) {
+    //         break;
     //     }
     //     i++;
     //     j--;
     // }
-    // 4. 二分法优化，这里要用开区间的写法来写
-    // i这个值的唯一性，所以二分其实是在找极限的成立值，这里对比的target其实就不是固定的一个值了，而是动态的条件
+
+    // 二分做法（统一用闭区间）
     let left = 0;
-    let right = m + 1;
-    while (left + 1 < right) {
-        // 这个i其实就相当于middle了
-        let i = Math.floor((left + right) / 2);
-        let j = Math.floor((m + n + 1) / 2) - i;
-        if (nums1[i] <= nums2[j + 1]) { // 这是在找极限，是一个动态的条件
-            left = i;
+    let right = m;
+    while (left <= right) {
+        i = Math.floor(left + right);
+        j = Math.floor((m + n + 1) / 2) - i;
+        if (nums1[i] <= nums2[j + 1]) {
+            left = i + 1; // 闭区间
         } else {
-            right = i;
+            right = i - 1; // 闭区间
         }
     }
-    // 推导一下就知道最终的i值是left
-    let i = left;
-    let j = Math.floor((m + n + 1) / 2) - i;
-    return (m + n) % 2 === 0 ? (Math.max(nums1[i], nums2[j]) + Math.min(nums1[i + 1], nums2[j + 1])) / 2 : Math.max(nums1[i], nums2[j]);
+
+    if ((m + n) % 2 !== 0) {
+        return Math.max(nums1[i], nums2[j]);
+    } else {
+        return (Math.max(nums1[i], nums2[j]) + Math.min(nums1[i + 1], nums2[j + 1])) / 2;
+    }
 };
