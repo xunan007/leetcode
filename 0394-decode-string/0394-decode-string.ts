18f17e4d6575c60aaa91ec6a852d34d799cd6cd6
绪南 <593855482@qq.com>
2026-01-11 16:47:43 +0800

    chore: 添加注释，变更版本

--- 0394-decode-string/0394-decode-string.ts
+++ 0394-decode-string/0394-decode-string.ts
@@ -1,41 +1,26 @@
 function decodeString(s: string): string {
-    // 先进栈，遇到]后开始出栈，直到遇到[
-    // 拼凑好字符串后再次入栈，直到遇到第二个[停下
-    // 另外这里还得处理数字大于10的情况
-    let stack = new Array();
-    let num = '0';
+    // 先进栈，然后遇到]开始出栈拼小段的字符
+    // 遇到[后找到前面的数字重组小段字符再进栈
+    const stack = [];
     for (let i = 0; i < s.length; i++) {
-        if (!isNaN(Number(s[i]))) {
-            num += s[i];
-        } else {
-            // 数字入栈
-            if (num !== '0') {
-                stack.push(Number(num));
-                num = '0';
+        stack.push(s[i]);
+        if (stack[stack.length - 1] === ']') {
+            stack.pop();
+            let str = '';
+            while (stack[stack.length - 1] !== '[') {
+                str = stack[stack.length - 1] + str;
+                stack.pop();
             }
-            // 非 ] 入栈
-            if (s[i] !== ']') {
-                stack.push(s[i]);
-            } else {
-                // ] 出栈
-                // 连续出栈到遇到 [
-                let str = '';
-                let v = stack.pop();
-                while (v !== '[') {
-                    str = v + str;
-                    v = stack.pop();
+            stack.pop();
+            let num = '';
+            while (!isNaN(stack[stack.length - 1])) {
+                num = stack[stack.length - 1] + num;
+                stack.pop();
+            }
+            if (num !== '') {
+                for (let i = 0; i < Number(num); i++) {
+                    stack.push(...(str.split('')));
                 }
-                // 如果前面是数字
-                if (!isNaN(stack[stack.length - 1])) {
-                    let result = '';
-                    let nums = stack.pop();
-                    for (let i = 0; i < nums; i++) {
-                        result += str;
-                    }
-                    str = result;
-                }
-                // 把字符串再推回去
-                stack.push(str);
             }
         }
     }
