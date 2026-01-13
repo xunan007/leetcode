df19eb46b875cc3045c4d638d76b91b53045b2ba
绪南 <593855482@qq.com>
2026-01-13 16:28:00 +0800

    chore: 添加注释，变更版本

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.ts
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.ts
@@ -1,4 +1,8 @@
 function canPartition(nums: number[]): boolean {
+    // 转换成背包问题，能不能凑出一个数组是 sum/2
+    // sum是自己累加的，如果能凑出 sum/2，那么另外一个数组自然也是 sum/2
+    // 所以转换成 dp 的定义是
+    // dp[i][j] 在容量为j时（最大为sum/2），前i个数是否能填满这个背包（不一定要全选）
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
@@ -6,41 +10,41 @@ function canPartition(nums: number[]): boolean {
     if (sum % 2 !== 0) {
         return false;
     }
+
     sum = sum / 2;
-    // // 一般做法：二维数组
-    // // 初始化dp数组
-    // // dp[i][j]的含义是：前i个物品中，能不能装满容量是j的背包
+
+    // i=0表示的是前0个，所以需要多补一行
     // let dp: boolean[][] = [];
     // for (let i = 0; i <= nums.length; i++) {
-    //     dp.push(new Array(sum+1));
+    //     dp.push(new Array(sum + 1));
+    //     // 容量为0当然都能装满，处理第一列
     //     dp[i][0] = true;
     // }
-    // for (let j = 0; j <= sum; j++) {
+    // for (let j = 1; j <= sum; j++) {
     //     dp[0][j] = false;
     // }
+
+    // // 有两个状态：1-物品 2-背包
     // for (let i = 1; i <= nums.length; i++) {
     //     for (let j = 1; j <= sum; j++) {
-    //         let noSelect = dp[i - 1][j];
-    //         let select = false;
-    //         if (j - nums[i - 1] >= 0) {
-    //             select = dp[i - 1][j - nums[i - 1]];
-    //         }
-    //         dp[i][j] = select || noSelect;
+    //         // 选 - 减掉这个容量后，这个东西之前的物品能够装满
+    //         // 不选 - 前面的本来就能满足了
+    //         dp[i][j] = (dp[i - 1][j - nums[i - 1]] || false) || (dp[i - 1][j] || false);
     //     }
     // }
-    // return dp[nums.length][sum];
-    // 滚动数组优化
-    let dp: boolean[] = new Array(sum + 1);
+
+    // 二维数组降一维度
+    let dp: boolean[] = new Array(sum + 1).fill(false);
     dp[0] = true;
-    for (let j = 1; j <= sum; j++) {
-        dp[j] = false;
-    }
     for (let i = 1; i <= nums.length; i++) {
         for (let j = sum; j >= 1; j--) {
-            if (j - nums[i - 1] >= 0) {
-                dp[j] = dp[j] || dp[j - nums[i-1]];
-            }
+            // 选 - 减掉这个容量后，这个东西之前的物品能够装满
+            // 不选 - 前面的本来就能满足了
+            dp[j] = (dp[j - nums[i - 1]] || false) || (dp[j] || false);
         }
     }
-    return dp[sum];
+    return dp[dp.length - 1];
+
+
+    // return dp[nums.length][sum];
 };
