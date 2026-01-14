9f429b40517604069c33cc7441e61453616f6dcb
绪南 <593855482@qq.com>
2026-01-14 21:55:47 +0800

    chore: 添加注释，变更版本

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,9 +1,18 @@
 function maxProfit(prices: number[]): number {
-    let hold = -Infinity;
-    let noHold = 0;
-    for (let i = 1; i <= prices.length; i++) {
-        noHold = Math.max(noHold, hold + prices[i - 1]);
-        hold = Math.max(hold, -prices[i - 1]);
+    // dp[i][j] 表示在i天，持有或不持有股票的最大收益
+    let dp = [];
+    for (let i = 0; i < prices.length; i++) {
+        dp.push(new Array(2).fill(0));
     }
-    return noHold;
+    dp[0][0] = 0; // 0-不持有
+    dp[0][1] = -prices[0]; // 1-持有
+    for (let i = 1; i < prices.length; i++) {
+        // 今天不持有=昨天持有+今天卖||昨天也不持有
+        // 今天持有=昨天持有||昨天没有持有(因为这里只能买卖一次，所以不能有前面的累积，只能是0)+今天买
+        // 第二个式子，如果前面是dp[i-1][0]-prices[i]，就变成多次买卖了
+        dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+        dp[i][1] = Math.max(-prices[i], dp[i - 1][1]);
+    }
+    // 最后不持有利润肯定是最大的
+    return dp[prices.length - 1][0];
 };
