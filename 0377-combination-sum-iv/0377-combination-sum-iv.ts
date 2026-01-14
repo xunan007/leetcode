4abeb94a85511131e5c9b145d8154f158a1c452b
绪南 <593855482@qq.com>
2026-01-14 20:46:56 +0800

    chore: 添加注释，变更版本

--- 0377-combination-sum-iv/0377-combination-sum-iv.ts
+++ 0377-combination-sum-iv/0377-combination-sum-iv.ts
@@ -1,11 +1,11 @@
 function combinationSum4(nums: number[], target: number): number {
-    let dp: number[] = new Array(target + 1).fill(0);
+    // 这道题直接用爬楼梯的思想来解题
+    let dp = new Array(target + 1).fill(0);
     dp[0] = 1;
-    // 注意这里是先遍历容量再遍历物品
-    for (let i = 1; i <= target; i++) {
-        for (let j = 1; j <= nums.length; j++) {
-            if (i - nums[j - 1] >= 0) {
-                dp[i] += dp[i - nums[j - 1]];
+    for (let j = 1; j <= target; j++) {
+        for (let i = 0; i < nums.length; i++) {
+            if (j - nums[i] >= 0) {
+                dp[j] += dp[j - nums[i]];
             }
         }
     }
