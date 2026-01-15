e75924127ca2dd4169b7b7a1908f51f03e3c8e79
绪南 <593855482@qq.com>
2026-01-15 16:01:48 +0800

    chore: 添加注释，变更版本

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.ts
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.ts
@@ -1,38 +1,28 @@
 function longestValidParentheses(s: string): number {
-    if (s.length === 0 || s.length === 1) {
-        return 0;
-    }
-    // 用枚举选谁的思路来做，这里枚举选谁特别依赖于对括号匹配连续性的理解
-    let dp: number[] = new Array(s.length).fill(0);
-    let result: number = 0;
-    // 初始化
-    if (s[0] === '(') {
-        if (s[1] === ')') {
-            dp[1] = 2;
-            result = 2;
-        }
-    } else {
-        dp[1] = 0;
-    }
-    for (let i = 2; i < s.length; i++) {
+    // https://leetcode.cn/problems/longest-valid-parentheses/solutions/314827/shou-hua-tu-jie-zhan-de-xiang-xi-si-lu-by-hyj8/?envType=study-plan-v2&envId=top-100-liked
+    // dp[i]的定义是以当前符号为结尾的最长有效括号子串长度
+    let dp = new Array(s.length).fill(0);
+    let max = 0;
+    for (let i = 1; i < s.length; i++) {
         if (s[i] === '(') {
+            // 第一种情况
             dp[i] = 0;
         } else {
+            // 第二种情况：()
             if (s[i - 1] === '(') {
-                dp[i] = dp[i - 2] + 2;
-            } else {
+                dp[i] = (dp[i - 2] || 0) + 2;
+                max = Math.max(max, dp[i]);
+            }
+            // 第三种情况（最难理解就是这种情况）：))
+            // ()) (()) (()())
+            // 直接跨过长度即可
+            if (s[i - 1] === ')') {
                 if (s[i - dp[i - 1] - 1] === '(') {
-                    if (i - dp[i - 1] - 1 === 0) {
-                        dp[i] = dp[i - 1] + 2;
-                    } else {
-                        dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
-                    }
-                } else {
-                    dp[i] = 0;
+                    dp[i] = (dp[i - dp[i - 1] - 2] || 0) + dp[i - 1] + 2;
+                    max = Math.max(max, dp[i]);
                 }
             }
         }
-        result = Math.max(result, dp[i]);
     }
-    return result;
+    return max;
 };
