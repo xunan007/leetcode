59120943b2a58b244bb07ac36b653d92c96cafef
绪南 <593855482@qq.com>
2026-01-19 22:32:34 +0800

    chore: 添加注释，变更版本
    Refactor jump function to improve logic and remove unnecessary steps.

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,23 +1,16 @@
 function jump(nums: number[]): number {
-    if (nums.length === 1) {
-        return 0;
-    }
-    let cover = nums[0] + 1;
-    let maxCover = nums.length;
-    let path: number[] = [cover];
-    if (cover >= maxCover) {
-        return path.length;
-    }
-    for (let i = 0; i < maxCover && i < path[path.length - 1]; i++) {
-        cover = Math.max(i + 1 + nums[i], cover);
-        if (cover >= maxCover) {
-            path.push(cover);
-            break;
-        }
-        // 下一步能走到的最大值已经定下来了，那么第二步就可以先跳过去了
-        if (i === path[path.length - 1] - 1) {
-            path.push(cover);
+    // 在一段里面找下一段的最大距离
+    // 一旦超过这一段，那么就是跳步
+    let border = 0;
+    let maxPos = 0;
+    for (let i = 0; i < nums.length - 1; i++) { 
+        // 因为跳的目标是最后一个，所以最后一个步数如何不用管
+        // 并且因为是一定能到达，所以也不用担心到达不了，只需要计算跳了几段就可以了
+        maxPos = Math.max(maxPos, nums[i] + i);
+        if (i === border) {
+            border = maxPos;
+            step++;
         }
     }
-    return path.length;
+    return step;
 };
