45b3b7b265c45bbf186a868ef1ab9aafc0f5eddd
绪南 <593855482@qq.com>
2026-01-20 16:33:08 +0800

    chore: 添加注释，变更版本

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,20 +1,19 @@
 function majorityElement(nums: number[]): number {
+    // 严格众数，众数的次数-其余数的总次数>0
+    // 投票，同一个人投赞成票，不是同一个人则投反对票，最后票数一定是大于 0 的
+    let result = 0;
     let votes = 0;
-    let num = null;
-    for (const x of nums) {
-        if (num === null) {
-            num = x;
+    for (let i = 0; i < nums.length; i++) {
+        if (votes === 0) {
             votes++;
+            result = nums[i];
         } else {
-            if (x === num) {
+            if (result === nums[i]) {
                 votes++;
             } else {
                 votes--;
-                if (votes === 0) {
-                    num = null;
-                }
             }
         }
     }
-    return num;
+    return result;
 };
