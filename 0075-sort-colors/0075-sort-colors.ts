a2305bfe57f3f382fed84cc11a809cc6a89732ac
绪南 <593855482@qq.com>
2026-01-20 19:32:44 +0800

    Refactor sortColors function with pointer method
    Refactor sortColors function to use a new approach with pointers for sorting colors in-place.

--- 0075-sort-colors/0075-sort-colors.ts
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -2,50 +2,17 @@
  Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums: number[]): void {
-    // 桶排序
-    // let numA = 0;
-    // let numB = 0;
-    // let numC = 0;
-    // const len = nums.length;
-    // for (let i = 0; i < len; i++) {
-    //     if (nums[i] === 0) {
-    //         numA++;
-    //     }
-    //     if (nums[i] === 1) {
-    //         numB++;
-    //     }
-    //     if (nums[i] === 2) {
-    //         numC++;
-    //     }
-    // }
-    // for (let i = 0; i < len; i++) {
-    //     if (numA) {
-    //         nums[i] = 0;
-    //         numA--;
-    //     } else if (numB) {
-    //         nums[i] = 1;
-    //         numB--;
-    //     } else {
-    //         nums[i] = 2;
-    //         numC--;
-    //     }
-    // }
-
-    // 双指针法
-    let p0 = 0;
-    let p2 = nums.length - 1;
-    let i = 0;
-    let j = 0;
-    while (i <= p2) { // 注意这里的越界条件
+    let i = 0; // [l, i] 表示为 1
+    let l = 0; // [0, l-1] 存的都是 0
+    let r = nums.length - 1;
+    while (i <= r) { // 这个区间里面的数都需要检查
         if (nums[i] === 0) {
-            [nums[i], nums[p0]] = [nums[p0], nums[i]];
-            p0++;
-            if (p0 > i) { // i要在p0和p2之间
-                i++;
-            }
+            [nums[i], nums[l]] = [nums[l], nums[i]];
+            l++;
+            i++; // i这个位置不需要再检测了，从左边换过来的一定是1（原地0的情况下，l和i是齐头并进的）
         } else if (nums[i] === 2) {
-            [nums[i], nums[p2]] = [nums[p2], nums[i]];
-            p2--;
+            [nums[i], nums[r]] = [nums[r], nums[i]];
+            r--; // 右边换过来的可能是2，所以需要再检查
         } else {
             i++;
         }
